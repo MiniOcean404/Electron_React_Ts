d390671efca897daa63a073a739448e0d9cb2ff7
戴海洋 <haiyang.dai@davincimotor.com>
2022-06-13 16:01:31 +0800

    fix: 因为 安装 react 开发者工具导致无法启动问题

--- src/main/config/create-window.ts
+++ src/main/config/create-window.ts
@@ -1,20 +1,19 @@
-import createMenu from '@/main/config/menu';
-import initTray from '@/main/config/tray';
-import { BrowserWindow } from 'electron';
-import { resolveHtmlPath } from '@/main/utils/path';
-import installExtensions from '@/main/config/dev-tools';
-import initBrowserWindowEvent from '@/main/life-cycle/browser-window';
 import { isDebug } from '@/constant/env';
 import AppUpdater from '@/main/config/auto-update';
 import browserWindowProp from '@/main/config/browser-window-prop';
+import installExtensions from '@/main/config/dev-tools';
 import initElectronRemote from '@/main/config/electron-remote';
 import { regGlobalShortcut } from '@/main/config/global-shortcut';
+import createMenu from '@/main/config/menu';
+import initTray from '@/main/config/tray';
+import initBrowserWindowEvent from '@/main/life-cycle/browser-window';
 import ipcMainInit from '@/main/message/accept';
+import { resolveHtmlPath } from '@/main/utils/path';
+import { BrowserWindow } from 'electron';
 
 export default async function createWindow() {
   const win: BrowserWindow | null = new BrowserWindow(browserWindowProp);
 
-  if (isDebug) await installExtensions();
   await win.loadURL(resolveHtmlPath('index.html'));
   initBrowserWindowEvent(win);
 
@@ -30,4 +29,9 @@ export default async function createWindow() {
   initTray(win);
   createMenu(win);
   regGlobalShortcut(win);
+
+  // objc[85955]: Class WebSwapCGLLayer is implemented in both xxxxx One of the two will be used. Which one is undefined.
+  // Failed to fetch extension, trying 4 more times
+  // 是因为安装开发者工具 没有外网
+  if (isDebug) await installExtensions();
 }
